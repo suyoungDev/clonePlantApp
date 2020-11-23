b4a4026793852b4b0712ad8ccf57cffdd6df1878
mochapoke <71932072+mochapoke@users.noreply.github.com>
2020-11-23 16:59:51 +0900

    추가: 뒤로가기버튼

--- Screens/PlantDetail.js
+++ Screens/PlantDetail.js
@@ -1,3 +1,4 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import {
   View,
@@ -129,9 +130,9 @@ const RequirementsBar = ({ icon, barPercentage }) => {
   )
 }
 
-const PlantDetail = () => {
+const PlantDetail = ({navigation}) => {
 
-  //Render
+  // Start of Render
   function renderRequirementsBar(){
     return(
       <RenderWrapper>
@@ -163,7 +164,6 @@ const PlantDetail = () => {
     )
   }
 
-  // render 2
   function renderRequirements(){
     return(
       <View 
@@ -206,24 +206,123 @@ const PlantDetail = () => {
 
   function renderFooter(){
     return(
-      <View style={{ flex: 1, flexDirection: 'row', paddingVertical: SIZES.padding, }}>
+      <View style={{ flex: 1, flexDirection: 'row', paddingVertical: SIZES.padding}}>
+        
         <TouchableOpacity
           style={{
-            width: '50%',
+            flex: 1,
+            // width: '50%',
             flexDirection: 'row',
             alignItems: 'center',
             justifyContent: 'center',
             borderTopRightRadius: 30,
-            borderTopRightRadius: 30,
+            borderBottomRightRadius: 30,
+            paddingHorizontal: SIZES.padding,
             backgroundColor: COLORS.primary
           }}
           onPress={() => {}}
         >
+          <Text style={{
+            color: COLORS.white,
+            ...FONTS.h2
+          }}>Take Action
+          </Text>
 
+          <Image 
+            source={icons.chevron}
+            resizeMode='contain'
+            style={{
+              marginLeft: SIZES.padding,
+              width: 20,
+              height: 20
+            }}
+          />
         </TouchableOpacity>
+
+        <View style={{ 
+          flex:1, 
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingHorizontal: SIZES.padding,
+          }}>
+            <Text style={{
+              flex: 1, 
+              color: COLORS.secondary,
+              ...FONTS.h4
+            }}>
+              Almost 2 weeks of growing time
+            </Text>
+            <Image 
+              source={icons.downArrow}
+              resizeMode='contain'
+              style={{
+                tintColor: COLORS.secondary,
+                marginLeft: SIZES.base,
+                width: 20,
+                height: 20,
+              }}
+            />
+        </View>
       </View>
     )
   }
+
+  function renderHeder(){
+    return(
+      <View style={{
+        position: 'absolute',
+        top: 50,
+        left: SIZES.padding,
+        right: SIZES.padding,
+      }}>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={{flex:1}}>
+            <TouchableOpacity
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 20,
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: 'rgba(255,255,255,0.5)'
+              }}
+              onPress={() => {navigation.goBack()}}
+            >
+              <Image 
+                source={icons.back}
+                resizeMode='contain'
+                style={{
+                  width: 20,
+                  height: 20
+                }}
+              />
+            </TouchableOpacity>
+          </View>
+          
+          <TouchableOpacity
+            style={{
+              flex:1,
+              alignItems: 'flex-end',
+              justifyContent: 'center'
+            }}
+            onPress={ () => {} }
+          >
+            <Image 
+              source={icons.focus}
+              resizeMode='contain'
+              style={{
+                width: 25,
+                height: 25
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+
+  // end of Render 
 
   return(
     <Container>
@@ -245,6 +344,8 @@ const PlantDetail = () => {
           {renderRequirements()}
           {renderFooter()}
       </RequirementsWrapper>
+
+      {renderHeder()}
     </Container>
   )
 }
